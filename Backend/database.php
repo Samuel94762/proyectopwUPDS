933ceafa2fbba1ea4d577d1aeeae9de0660bb844
diego260h <diegoorihuelaherrera@gmail.com>
2025-07-28 15:24:21 -0400

    Creacion de un nuevo directorio ene le directorio raiz llamado components

--- Backend/database.php
+++ Backend/database.php
@@ -8,13 +8,13 @@ $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASSWORD']);
 
 try {
     $conn = new PDO(
-        "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']}", 
+        "mysql:host={$_ENV['DB_HOST']};port=3307;dbname={$_ENV['DB_NAME']}", 
         $_ENV['DB_USER'], 
         $_ENV['DB_PASSWORD']
     );
     
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
+
 } catch(PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
